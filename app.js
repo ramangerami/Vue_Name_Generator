a39d327d2e8571c5a9b211dea146485b29f35558
ramangerami <raman_gerami@hotmail.com>
2021-05-02 21:53:10 -0700

    added name generator api

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        getUser() {
-            this.firstName = 'Rachel',
-            this.lastName = 'Smith',
-            this.email = 'rachelsmith@gmail.com',
-            this.gender = 'female',
-            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+            console.log(results)
+
+
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
         },
     },
 
